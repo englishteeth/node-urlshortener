7aedd0f782bab63182f045ee4cf614050f6d7d54
Glitch (fossil-citrine-transport) <none>
2020-04-29 09:46:12 +0000

    🚲🎓 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var express = require('express');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+
+var cors = require('cors');
+
+var bodyParser = require('body-parser'); 
+
+var app = express();
+
+// Basic Configuration 
+var port = process.env.PORT || 3000;
+
+/** this project needs a db !! **/ 
+mongoose.connect(process.env.DB_URI);
+
+app.use(cors());
+
+/** this project needs to parse POST bodies **/
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function(req, res){
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+// API endpoint... 
+app.post("/api/shorturl/new", (req, res)=>{
+  const { url } = req.body;
+  res.json({ url: `${url}` });
+});
+
+
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+
+app.listen(port, function () {
+  console.log('Node.js listening ...');
+});
